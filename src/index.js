b8b697d460f2b346012bad11c229635cfc83c30c
Hammad Naseem <hammadnsm@gmail.com>
2020-05-25 18:34:20 -0500

    added buzzer reset and disable buzzer functions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 const express = require('express')
 const http = require('http')
-const io = require('socket.io')
-
-const port = process.env.port || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-app.use(express.static(publicDirectoryPath))
+const port = process.env.port || 3000
+
+app.use(express.static('public'))
 
 io.on('connection', (socket) => {
+    console.log('New connection')
 
-}) 
+    socket.on('sendMessage', (message, callback) => {
+        io.emit('incomingMessage', message)
+        callback()
+    })
+
+    socket.on('buzzerPressed', (name) => {
+        buzzed(name)
+    })
+
+    socket.on('resetBuzzer', () => {
+        resetBuzzer()
+    })
+})
 
 server.listen(port, () => console.log(`listening on port ${port}`))
+
+function buzzed(name) {
+    io.emit('buzzerPressed', name, true)
+}
+
+function resetBuzzer() {
+    io.emit('resetBuzzer')
+}
