acb7dd3041b8021078dbd0577a95fa2350cce2e3
hdawg5000 <hammadnsm@gmail.com>
2020-05-30 01:23:35 -0500

    add express routes for each page

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const fs = require('fs')
+const path = require('path')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
 let players = new Map()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 const port = process.env.port || 3000
 
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(publicDirectoryPath + '/index.html')
+})
+
+app.get('/buzzer', (req, res) => {
+    res.sendFile(publicDirectoryPath + '/buzzer.html')
+})
+
+app.get('/admin', (req, res) => {
+    res.sendFile(publicDirectoryPath + '/admin.html')
+})
 
 io.on('connection', (socket) => {
     console.log('New connection', socket.id)
