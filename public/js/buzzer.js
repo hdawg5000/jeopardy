5cc560c1ca966eb819fd92b9bb80bf856c338d87
Hammad Naseem <hammadnsm@gmail.com>
2020-05-29 22:16:03 -0500

    Keep track of connected users

--- public/js/buzzer.js
+++ public/js/buzzer.js
@@ -1,20 +1,31 @@
 const socket = io()
+const Constants = '../constants/constants'
 
 let buzzerDisabled = false
+let playerName = ''
+document.querySelector('#buzzerForm').style.display = 'none'
+
+document.querySelector('#name-form')
+    .addEventListener('submit', (e) => {
+        e.preventDefault()
+        playerName = document.querySelector('#name-input').value
+        console.log('player', playerName)
+        socket.emit('submittedName', playerName)
+        document.querySelector('#name-form').style.display = 'none'
+        document.querySelector('#buzzerForm').style.display = 'block'
+    })
 
 document.querySelector('#buzzerForm')
     .addEventListener('submit', (e) => {
         e.preventDefault()
-        const name = document.querySelector('input').value
         socket.emit('buzzerPressed', name)
-
     })
 
-socket.on('buzzerPressed', (name, disableButton) => {
+socket.on('buzzerPressed', (name) => {
     console.log(`${name} buzzed!`)
-    document.querySelector('button').setAttribute('disabled', '')
+    document.querySelector('#buzzer-btn').setAttribute('disabled', '')
 })
 
 socket.on('resetBuzzer', () => {
-    document.querySelector('button').removeAttribute('disabled')
+    document.querySelector('#buzzer-btn').removeAttribute('disabled')
 })
--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
+const fs = require('fs')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
+let players = new Map()
 
 const port = process.env.port || 3000
 
 app.use(express.static('public'))
 
 io.on('connection', (socket) => {
-    console.log('New connection')
+    console.log('New connection', socket.id)
+
+    socket.on('submittedName', (name) => {
+        console.log('name', name)
+        players.set(socket.id, name)
+        fs.appendFile('src/users.txt', `${name},${socket.id}`, () => console.log('success'))
+    })
 
     socket.on('sendMessage', (message, callback) => {
         io.emit('incomingMessage', message)
         callback()
     })
 
-    socket.on('buzzerPressed', (name) => {
+    socket.on('buzzerPressed', () => {
+        const name = players.get(socket.id)
         buzzed(name)
     })
 
@@ -30,7 +40,8 @@ io.on('connection', (socket) => {
 server.listen(port, () => console.log(`listening on port ${port}`))
 
 function buzzed(name) {
-    io.emit('buzzerPressed', name, true)
+    console.log('emitting', name)
+    io.emit('buzzerPressed', name)
 }
 
 function resetBuzzer() {
