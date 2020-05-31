01421d50af9471d84b550e210864bf1a60bf1c82
hdawg5000 <hammadnsm@gmail.com>
2020-05-30 20:56:52 -0500

    start of user tracking

--- public/js/admin.js
+++ public/js/admin.js
@@ -6,3 +6,33 @@ document.querySelector('#controls')
 
         socket.emit('resetBuzzer')
     })
+
+socket.on('connect', () => {
+    socket.emit('adminConnected')
+})
+
+socket.on('userLeft', (name) => {
+    console.log(name, 'left')
+})
+
+socket.on('buzzerPressed', (name) => {
+    document.querySelector('#player-name-buzzed').setAttribute(textContent, `${name} buzzed!`)
+})
+
+socket.on('userConnected', (name, socketId) => {
+    console.log(name, socketId)
+    let li = document.createElement('li')
+    li.textContent = `${name} (${socketId})\n`
+    document.querySelector('#player-list')
+        .appendChild(li)
+})
+
+socket.on('connectedPlayers', (players) => {
+    console.log('connected', players.keys)
+    // players.keys().map(key => {
+    //     let li = document.createElement('li')
+    //     li.textContent = `${players.get(key)} (${key})\n`
+    //     document.querySelector('#player-list')
+    //         .appendChild(li)
+    // })
+})
